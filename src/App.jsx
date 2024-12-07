ad9a7425fe6a74911031f66c570edfbade251082
Agner <80971302+AgnerShimokawa@users.noreply.github.com>
2024-12-08 01:46:57 +0900

    Erased unnecessary comments App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,6 @@ import './App.css'
 import CapitalizingAndColoring from './components/capitalizing-and-coloring/capitalizing-and-coloring'
 import Button from './components/button/button'
 
-/*
-Um componente que colore um parágrafo e deixe tudo com letra maiúsula. 
-O componente precisa renderizar na tela um parágrafo colorido e capitalizado.
-para colorir os texto é preciso usar as props.
-pode utilizar o style inline para colorir os parágrafos
-*/
-
-
-
 function App() {
   return (
     <>
